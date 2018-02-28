e03879f31bb6f5940df2d0dbcfa40a6c7d744a1f
Diogo F. Guzenski <diogofraga132@hotmail.com>
2018-02-28 13:51:12 -0300

    1
    first update

---
+++ pt1/script.js
@@ -0,0 +1,5 @@
+function AlertaEssaPoha(){
+	var text = document.getElementById("inputText").value;
+	alert(`Mensagem: ${text} | 2+2 = ${2+2} ;) 
+		DOM = interface pelo qual HTML e XML são interpretados pelo browser`);
+}
